e24b1bb942b59483d16ae21e7cfe5804d12ef8c1
Nikita <Nikita.yavkin92@gmail.com>
2023-04-19 09:54:49 +0300

    updating the database and adding slugs

--- sitehotel/touristapp/urls.py
+++ sitehotel/touristapp/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('about/', about, name='about'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('post/<int:post_id>/', show_post, name='post'),
+    path('post/<slug:post_slug>/', show_post, name='post'),
     path('category/<int:cat_id>/', show_category, name='category'),
 ]
--- sitehotel/touristapp/views.py
+++ sitehotel/touristapp/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import *
 
@@ -28,8 +28,17 @@ def contact(request):
     return HttpResponse('Обратная связь')
 
 
-def show_post(request, post_id):
-    return HttpResponse(f'Отображение статьи с id = {post_id}')
+def show_post(request, post_slug):
+    post = get_object_or_404(Apartaments, slug=post_slug)
+
+    context = {
+        'post': post,
+        'menu': menu,
+        'title': post.title,
+        'cat_selected': post.cat_id,
+    }
+
+    return render(request, 'touristapp/post.html', context=context)
 
 
 def show_category(request, cat_id):
